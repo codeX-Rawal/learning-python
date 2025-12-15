55a99cc133a090f6997a5348e23bdb328e409730
Rawal Singh <rawalsinghr798@gmail.com>
2025-12-15 11:51:31 +0530

    Day 1: Python introduction and first program

---
+++ hello.py
@@ -0,0 +1,14 @@
+# Day 1: Introduction to Python
+# My first Python program
+
+print("Hello World!")
+print("Welcome to my Python learning journey 🚀")
+
+# Taking user input
+name = input("Enter your name: ")
+print("Hello,", name)
+
+# Simple calculation
+a = 10
+b = 5
+print("Sum of a and b is:", a + b)
